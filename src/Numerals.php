e55c0cdc7140880aede0f8163db103c46fc5e707
Kevin Cittadini <cittadini.kevin@gmail.com>
2018-10-30 18:32:30 +0100

    Test tens roman/arab digits.

--- src/Numerals.php
+++ src/Numerals.php
@@ -4,8 +4,13 @@ namespace Numerals;
 
 class Numerals
 {
+
     const ARAB_TO_ROMAN_TABLE = [
-        1 => 'I',
+        1  => 'I',
+        4  => 'IV',
+        5  => 'V',
+        9  => 'IX',
+        10 => 'X'
     ];
 
     /**
@@ -19,6 +24,19 @@ class Numerals
             return false;
         }
 
-        return self::ARAB_TO_ROMAN_TABLE[$arabNumeral];
+        if (isset(self::ARAB_TO_ROMAN_TABLE[$arabNumeral])) {
+            return self::ARAB_TO_ROMAN_TABLE[$arabNumeral];
+        }
+
+        $result       = '';
+        $numbersTable = array_reverse(self::ARAB_TO_ROMAN_TABLE, true);
+
+        foreach ($numbersTable as $arabKey => $romanNumeral) {
+            for (; $arabNumeral >= $arabKey; $arabNumeral -= $arabKey) {
+                $result .= $romanNumeral;
+            }
+        }
+
+        return $result;
     }
 }
--- tests/NumeralsTest.php
+++ tests/NumeralsTest.php
@@ -20,10 +20,17 @@ class NumeralsTest extends TestCase
     /**
      * @test
      */
-    public function simpleArabToRoman()
+    public function tensArabToRoman()
     {
         $this->assertEquals('I', Numerals::toRoman(1));
         $this->assertEquals('II', Numerals::toRoman(2));
         $this->assertEquals('III', Numerals::toRoman(3));
+        $this->assertEquals('IV', Numerals::toRoman(4));
+        $this->assertEquals('V', Numerals::toRoman(5));
+        $this->assertEquals('VI', Numerals::toRoman(6));
+        $this->assertEquals('VII', Numerals::toRoman(7));
+        $this->assertEquals('VIII', Numerals::toRoman(8));
+        $this->assertEquals('IX', Numerals::toRoman(9));
+        $this->assertEquals('X', Numerals::toRoman(10));
     }
 }
